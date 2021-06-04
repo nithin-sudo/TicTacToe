af2a91e03041c9fdf824940ea89d091059f3374b
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-04 17:32:40 +0530

    Creating a board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,21 @@
 package tictactoe;
 public class TicTacToeGame {
+	static char[] Board;
 	public static void main(String[] args){
 		System.out.println("Welcome to Tic Tac Toe Game");
+		createBoard();
 	}
+	
+	/**({@summary}
+	 * Creating a board
+	 */
+	public static void createBoard()
+	{
+		Board = new char[9];
+		for (int i = 0; i < 9; i++) {
+			Board[i] = '-';
+		}
+	}
+	
+	
 }

2f7920085d050f206f050c01476d2d6b6ac312fb
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-04 18:36:07 +0530

    Asking Player choose between 'x' and 'o'

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,11 @@
 package tictactoe;
+import java.util.Scanner;
 public class TicTacToeGame {
 	static char[] Board;
 	public static void main(String[] args){
 		System.out.println("Welcome to Tic Tac Toe Game");
 		createBoard();
+		userChoice();
 	}
 	
 	/**({@summary}
@@ -16,6 +18,23 @@ public class TicTacToeGame {
 			Board[i] = '-';
 		}
 	}
-	
-	
+	 // Asking User to choose between X and O	
+	public static void userChoice() 
+	{
+		System.out.println("Enter the character 'x' or 'o':");
+		Scanner sc = new Scanner(System.in);
+		char ch= sc.next(). charAt(0);
+		if(ch =='x'|| ch =='o')
+		{
+			if(ch=='x')
+				System.out.println("Player chosen"+ch+"Computer is o");
+			else
+				System.out.println("Player chosen "+ch+" Computer is x");
+		}
+		else
+		{
+			System.out.println("Enter Valid input:x or o");
+			userChoice();
+		}
+	}
 }

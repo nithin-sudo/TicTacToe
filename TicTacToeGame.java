2f56efaadcfb91892e808097b5e9a402faafa370
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-07 11:27:34 +0530

    Added:ComputerFirstTurn

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -14,26 +14,20 @@ public class TicTacToeGame {
 	static char[] Board;
 	static char ch;
 	static Scanner sc = new Scanner(System.in);
+	static char comp;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
+		createBoard();
+		printBoard();
 		userChoice();
 		
 		
 		// Calling to Create a Board
-		createBoard();
 
 		// Asking user to choose x or o
-		
-		// Displaying board layout
-		printBoard();
 
-		// Displaying Board for User to Choose
-		userBoard();
-		
-		userMove();
-		
-		
+		// Displaying board layout
 	}
 
 	/**
@@ -54,13 +48,16 @@ public class TicTacToeGame {
 		System.out.println("Enter the character 'x' or 'o':");
 		ch = sc.next().charAt(0);
 		if (ch == 'x' || ch == 'o') {
-			if (ch == 'x')
-			{
+			if (ch == 'x') {
+				comp='o';
 				System.out.println("Player chosen " + ch + " Computer is o");
 				whoPlaysFirst();
-			}
-			else
+			} else {
+				 comp='x';
 				System.out.println("Player chosen " + ch + " Computer is x");
+				//System.out.println("player lost in toss computer number is");
+				//computerTurn();
+			}
 		} else {
 			System.out.println("Enter Valid input:x or o");
 			userChoice();
@@ -97,7 +94,7 @@ public class TicTacToeGame {
 	 * User making a move by seeing the userBoard
 	 */
 	public static void userMove() {
-		System.out.println("\nEnter a slot number :\n");
+		System.out.println("\nPlayer's turn Enter a slot number :\n");
 		int number = sc.nextInt();
 		if (number > 0 && number <= 9 && Board[number] != 'x' && Board[number] != 'o') {
 			Board[number] = ch;
@@ -110,26 +107,36 @@ public class TicTacToeGame {
 	}
 
 	public static void whoPlaysFirst() {
-		double toss = (Math.floor(Math.random() * 10) % 2)+1;
+		double toss = (Math.floor(Math.random() * 10) % 2) + 1;
 		System.out.println("toss is :" + toss);
 		System.out.println("choose your choice 1.head 2.tail");
 		int choice = sc.nextInt();
 		int x;
-		if(toss==choice) 
-			x=1;
+		if (toss == choice)
+			x = 1;
 		else
-			x=0;
+			x = 2;
 		switch (x) {
 		case 1:
-				System.out.println("player's turn");
-				userChoice();
-				break;
-		case 0:
-				System.out.println("computer's turn");
-				break;
+			System.out.println("player's turn");
+			userMove();
+			break;
+		case 2:
+			System.out.println("computer's turn");
+			computerFirstTurn();
+			break;
 		default:
 			System.out.println("invalid input");
 			whoPlaysFirst();
 		}
+	}
+
+	public static void computerFirstTurn() {
+		int[] computer= {1,3,7,9};
+		double index=(Math.floor(Math.random() * 10) % 4);
+		int x=computer[(int) index];
+		Board[x]=comp;
+		userBoard();
+		
 	}
 }
